39be26079d8995bb50d5e89e307a42b78907915b
u88allz <41806246-u88allz@users.noreply.replit.com>
2025-06-26 15:31:44 +0000

    Restored to 'c6ea016fe7d5832a008956bdaf5789e6dd548a71'
    Replit-Restored-To: c6ea016fe7d5832a008956bdaf5789e6dd548a71

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./compiled.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
